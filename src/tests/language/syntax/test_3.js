0c234ea9b8f8c8c0297ce55a401893abcea0f506
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2017-03-02 23:58:31 +0200

    Add templates for tests.

---
+++ src/tests/language/syntax/test_3.js
@@ -0,0 +1,255 @@
+angular.module('llt.app')
+  .constant('LANGUAGE_MORFOLOGY_', [
+    {
+      question: '',
+      answers: [
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: '',
+          correct: true
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        }
+      ]
+    },
+    {
+      question: '',
+      answers: [
+        {
+          text: '',
+          correct: true
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        }
+      ]
+    },
+    {
+      question: '',
+      answers: [
+        {
+          text: '',
+          correct: true
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        }
+      ]
+    },
+    {
+      question: '',
+      answers: [
+        {
+          text: '',
+          correct: true
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        }
+      ]
+    },
+    {
+      question: '',
+      answers: [
+        {
+          text: '',
+          correct: true
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        }
+      ]
+    },
+    {
+      question: '',
+      answers: [
+        {
+          text: '',
+          correct: true
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        }
+      ]
+    },
+    {
+      question: '',
+      answers: [
+        {
+          text: '',
+          correct: true
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        }
+      ]
+    },
+    {
+      question: '',
+      answers: [
+        {
+          text: '',
+          correct: true
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        }
+      ]
+    },
+    {
+      question: '',
+      answers: [
+        {
+          text: '',
+          correct: true
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        }
+      ]
+    },
+    {
+      question: '',
+      answers: [
+        {
+          text: '',
+          correct: true
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        }
+      ]
+    },
+    {
+      question: '',
+      answers: [
+        {
+          text: '',
+          correct: true
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        }
+      ]
+    },
+    {
+      question: '',
+      answers: [
+        {
+          text: '',
+          correct: true
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        }
+      ]
+    }
+  ]);
