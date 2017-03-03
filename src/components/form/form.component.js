327008500aefbdd398f8ed073d5df4ed36ff3f5b
ivanna-ostrovets <ivanna.ostrovets@gmail.com>
2017-03-03 19:34:14 +0200

    Change test view.

--- src/components/form/form.component.js
+++ src/components/form/form.component.js
@@ -1,13 +1,27 @@
-function FormController(
-  $routeParams,
-  tests
-) {
+function FormController($routeParams,
+                        tests) {
   this.tests = tests;
   this.test = this.tests.get($routeParams.id);
+
+  this.range = function (min, max, step) {
+    min = min || 1;
+    max = max || 12;
+    step = step || 1;
+
+    var input = [];
+    for (var i = min; i <= max; i += step) {
+      input.push(i);
+    }
+    return input;
+  };
+
+  $(document).ready(function () {
+    $('ul.tabs').tabs();
+  });
 }
 
 angular.module('llt.app')
-  .component('lltForm', {
-    templateUrl: 'components/form/form.component.html',
-    controller: FormController
-  });
+    .component('lltForm', {
+      templateUrl: 'components/form/form.component.html',
+      controller: FormController
+    });
