935d38f02a4dfb9cfca73f2de07d1eb8a0409a65
ivanna-ostrovets <ivanna.ostrovets@gmail.com>
2017-03-06 12:15:26 +0200

    Make more tests JSON.

--- src/tests.service.js
+++ src/tests.service.js
@@ -3,6 +3,20 @@ function TestsService(
 ) {
 	this.tests = {
 		'language_morfology_1' : LANGUAGE_MORFOLOGY_1,
+		'language_morfology_2' : LANGUAGE_MORFOLOGY_2,
+		'language_morfology_3' : LANGUAGE_MORFOLOGY_3,
+		'language_morfology_4' : LANGUAGE_MORFOLOGY_4,
+		'language_morfology_5' : LANGUAGE_MORFOLOGY_5,
+		'language_syntax_1' : LANGUAGE_SYNTAX_1,
+		'language_syntax_2' : LANGUAGE_SYNTAX_2,
+		'language_syntax_3' : LANGUAGE_SYNTAX_3,
+		'language_syntax_4' : LANGUAGE_SYNTAX_4,
+		'language_syntax_5' : LANGUAGE_SYNTAX_5,
+		'literature_1' : LITERATURE_1,
+		'literature_2' : LITERATURE_2,
+		'literature_3' : LITERATURE_3,
+		'literature_4' : LITERATURE_4,
+		'literature_5' : LITERATURE_5
 	}
 }
 
