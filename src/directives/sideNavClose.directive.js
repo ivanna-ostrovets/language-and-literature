594a371a6a574986719feef243d8a6e7c2347e5b
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2017-03-08 18:54:33 +0200

    Fix bug with expanded collapsible after closing sideNav

--- src/directives/sideNavClose.directive.js
+++ src/directives/sideNavClose.directive.js
@@ -5,6 +5,11 @@ angular.module('llt.app')
         link: function(scope, elem, attrs) {
           elem.on('click', function() {
             $(attrs.sideNavClose).sideNav('hide');
+
+            $(".collapsible-header").removeClass(function(){
+              return "active";
+            });
+            $(".collapsible").collapsible({accordion: false});
           })
         }
       }
