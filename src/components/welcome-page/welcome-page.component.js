88470aa592d51903b4aaa78bdf6a7841f3a86899
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2017-03-18 20:30:10 +0200

    Fix start test button not opening sideNav correctly

--- src/components/welcome-page/welcome-page.component.js
+++ src/components/welcome-page/welcome-page.component.js
@@ -2,10 +2,6 @@ import './welcome-page.component.scss';
 import template from './welcome-page.component.html';
 
 function WelcomePageController() {
-  $(document).ready(function(){
-    $(".start-test").sideNav();
-    $('.collapsible').collapsible();
-  });
 }
 
 angular.module('llt.app')
